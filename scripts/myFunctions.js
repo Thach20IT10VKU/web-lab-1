a3e9c88214b527ec733b3a699fcb72755fb87ed3
Thach20IT10VKU <89327179+Thach20IT10VKU@users.noreply.github.com>
2023-02-12 17:05:10 +0700

    Chap 10

---
+++ scripts/myFunctions.js
@@ -0,0 +1,24 @@
+/* Hoang Ngoc Thach
+11/02/2022
+My Function List
+*/
+
+//Thank you alert
+function tyAlert() {
+    alert("Thank you. We will contact you soon.");
+}
+
+//Video 1 window
+function video1() {
+    window.open("https://www.gimp.org/", "_blank", "width=610, height=360");
+}
+
+//Video 2 window
+function video2() {
+    window.open("https://www.python.org/downloads/", "_blank", "width=610, height=360");
+}
+
+//Video 3 window
+function video3() {
+    window.open("https://www.mozilla.org/vi/firefox/new/", "_blank", "width=610, height=360");
+}
